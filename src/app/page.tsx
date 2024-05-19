93d1c0ac9f5b69bcee338c2544c1c9af88352969
fauziw97 <ahmadfauziw97@gmail.com>
2024-05-20 01:02:49 +0700

    use upload modal in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,27 @@
 'use client'
 
+import React from 'react'
+import UploadModal from './component/UploadModal'
+
 export default function Home() {
+  const [selectedFile, setSelectedFile] = React.useState<string>('')
+  const [isModalOpen, setModalOpen] = React.useState<boolean>(false)
+
+
+  const closeModal = () => {
+    setModalOpen(false)
+  }
+
+  const onClickUpload = () => {
+    setModalOpen(true)
+  }
+
+  const onFileSelected = (url: string) => {
+    setSelectedFile(url)
+    closeModal()
+    return url
+  }
+
   return (
     <>
       <div className="absolute w-[400px] h-[400px] m-auto inset-0 p-4 rounded">
@@ -17,12 +38,18 @@ export default function Home() {
           </label>
           <textarea className="border-2 h-36 w-full" name="description" />
         </div>
+        <div className="mt-3">
+          <span className="block">File Selected : </span>
+          <span>{selectedFile}</span>
+        </div>
         <button
           className="border py-2 px-5 bg-blue-600 text-white rounded-md mt-5"
-          type="button">
+          type="button"
+          onClick={onClickUpload}>
           Upload
         </button>
       </div>
+      {isModalOpen && <UploadModal onFileSelected={onFileSelected} closeModal={closeModal} />}
     </>
   )
 }
