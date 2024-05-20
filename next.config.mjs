19bc79de808ecc00664d7ef18621098dac73e7e9
fauziw97 <ahmadfauziw97@gmail.com>
2024-05-20 09:25:10 +0700

    fix undefined file url

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    env: {
+      S3_BUCKET: process.env.S3_BUCKET,
+      S3_ACCESS_KEY: process.env.S3_ACCESS_KEY,
+      S3_SECRET_KEY: process.env.S3_SECRET_KEY,
+      S3_REGION: process.env.S3_REGION,
+      DIGITAL_OCEAN_ENDPOINT: process.env.DIGITAL_OCEAN_ENDPOINT,
+    },
+  }
+  
+  export default nextConfig
+  
